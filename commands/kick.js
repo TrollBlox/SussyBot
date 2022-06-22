df6af28bf3eb150ba88322ef8c89ba0fddb5fe2e
TrollBlox <caelbrenning@gmail.com>
2022-06-22 00:02:56 -0500

    kicking works and did something to ban, i forgor what

--- commands/kick.js
+++ commands/kick.js
@@ -3,19 +3,19 @@ const { MessageEmbed, Permissions } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ban')
-		.setDescription('Bans a user')
+		.setName('kick')
+		.setDescription('Kicks a user')
     .addUserOption(option =>
       option.setName('user')
-      .setDescription('The user you want to ban')
+      .setDescription('The user you want to kick')
       .setRequired(true))
     .addStringOption(option =>
       option.setName('reason')
-      .setDescription('The reason to ban that user')
+      .setDescription('The reason to kick that user')
       .setRequired(false)),
   async execute(int, client) {
     const embededd = new MessageEmbed()
-      .setTitle('Ban')
+      .setTitle('Kick')
       .setColor('#F21717');
   
     const func = require('../utils/functions');
@@ -23,26 +23,26 @@ module.exports = {
     const reason = int.options.getString('reason') || 'No reason provided';
 
     try {
-      if (!int.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+      if (!int.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) {
         embededd.setDescription('You do not have permission to use this command!');
         return int.reply({ embeds: [ embededd ]});
       }
       
       if (int.member.roles.highest > int.guild.members.cache.get(user.id).roles.highest) {
-        embededd.setDescription('You cannot ban this person!');
+        embededd.setDescription('You cannot kick this person!');
         return int.reply({ embeds: [ embededd ]});
       }
   
-      int.guild.members.ban(user.id, { reason: `${reason}` });
+      int.guild.members.kick(user.id, `${reason}`);
 
       func.log(int);
   
-      embededd.setDescription(`Successfully Banned <@${user.id}> for ${reason}!`);
+      embededd.setDescription(`Successfully kicked <@${user.id}> for ${reason}!`);
       return int.reply({ embeds: [ embededd ] });
       
     } catch (error) {
       func.error(error, int, client);
-      embededd.setDescription(`Something went wrong Banning <@${user.id}>!`);
+      embededd.setDescription(`Something went wrong kicking <@${user.id}>!`);
       return int.reply({ embeds: [ embededd ] });
     }
   },
